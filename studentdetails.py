dcfb69d8375fab44b141f5b5ffff9414ec46ee41
aakanksha-otari <44223836+aakanksha-otari@users.noreply.github.com>
2018-10-24 00:35:17 +0530

    Add files via upload

---
+++ studentdetails.py
@@ -0,0 +1,23 @@
+Name=input("Enter user name-")
+Rollnumber=input("Enter Roll number-")
+GR=input("Enter user G.R. number-")
+Mmarks=input("Enter maths' marks-")
+Emarks=input("Enter English's marks-")
+Smarks=input("Enter Science's marks-")
+
+Name=[]
+Name.append(input("Enter user name-"))
+Rollnumber=[]
+Rollnumber.append(input("Enter Roll number-"))
+GR=[]
+GR.append(input("Enter user G.R. number-"))
+Mmarks=[]
+Mmarks.append(input("Enter maths' marks-"))
+Emarks=[]
+Emarks.append(input("Enter English's marks-"))
+Smarks=[]
+Smarks.append(input("Enter Science's marks-"))
+
+	
+
+
